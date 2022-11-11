a0abd696c07b6fc109b49e150368e363793a022e
mabrams40 <maciej.abrams@gmail.com>
2022-11-11 08:57:19 +0100

    Minor fixes

--- Part 1/Calculator/script.js
+++ Part 1/Calculator/script.js
@@ -1,11 +1,12 @@
 function calculatorFormHandler(form, operationType){
-    let operationResult = document.getElementById("operationResult");
-    operationResult.innerHTML = calculate(form, operationType);
+    let result = document.getElementById("result");
+    result.innerHTML = calculate(form, operationType);
 }
 
 function calculate(form, operationType){
     let a = parseInt(form.number1.value);
     let b = parseInt(form.number2.value);
+    if(isNaN(a) || isNaN(b)) return "Type in both numbers.";
     switch(operationType){
         case "Add":
             return a+b;
@@ -15,7 +16,7 @@ function calculate(form, operationType){
             return a*b;
         case "Divide":
             if(b == 0){
-                return "You cannot divide by 0!";
+                return "You cannot divide by 0.";
             }
             return a/b;
     }
